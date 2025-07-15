face1c0bc0dcb371df825f0f599123000974ceec
Lynx Prjkt <secarikkertas160@gmail.com>
2025-07-16 03:49:48 +0700

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,9 @@
+from .start_handler import start
+from .xp_handler import xp_tracker
+from .stats_handler import stats
+from .leaderboard_handler import leaderboard
+from .challenge_handler import challenge, challenge_callback
+from .give_handler import give
+from .shop_handler import shop
+from .shop_callback import shop_callback
+from .surrender_handler import surrender
